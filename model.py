7ca4a85c15573c725c08a0457c2fa2106deea3b2
kebo <itachi971009@gmail.com>
2020-06-03 23:57:17 +0800

    [fea] add requirements.txt

---
+++ model.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@author: kebo
+@contact: itachi971009@gmail.com
+@version: 1.0
+
+@file: model.py 
+@time: 2020/6/3 下午11:45
+
+这一行开始写关于本文件的说明与解释
+
+"""
+import tensorflow as tf
+
+
+def func():
+    pass
+
+
+if __name__ == '__main__':
+    func()
